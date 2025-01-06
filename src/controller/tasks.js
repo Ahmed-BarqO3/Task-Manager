1250581ebc1719895d48176748de0e6c6a80dd20
AHMED <ahmed.barq03@gmail.com>
2025-01-06 12:57:13 +0200

    Add custom error handling for tasks

--- src/controller/tasks.js
+++ src/controller/tasks.js
@@ -1,47 +1,48 @@
 const Task = require('../models/Task');
 const wrap = require('express-async-wrapper');
+const  CustomError  = require('../errors/error')
 
 
-const getAllTasks = wrap(async (req, res) => {
+const getAllTasks = wrap(async (req, res, next) => {
 
   const tasks = await Task.find({});
   res.status(200).json({ tasks });
 
 })
 
-const getTask = wrap(async (req, res) => {
+const getTask = wrap(async (req, res, next) => {
 
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
   if (!task) {
-    return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    return next(new CustomError(`No task with id : ${taskID}`, 404));
   }
   res.status(200).json(task);
-
 })
-const createTask = wrap(async (req, res) => {
+
+const createTask = wrap(async (req, res, next) => {
 
   const task = await Task.create(req.body);
   res.status(201).json(task);
 });
 
-const deleteTask = wrap(async (req, res) => {
+const deleteTask = wrap(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
   if (!task) {
-    return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    return next(new CustomError(`No task with id : ${taskID}`, 404));
   }
   res.status(200).json({ msg: 'Task deleted' });
 });
 
-const updateTask = wrap(async (req, res) => {
+const updateTask = wrap(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
     new: true,
     runValidators: true
   });
   if (!task) {
-    return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    return next(new CustomError(`No task with id : ${taskID}`, 404));
   }
   res.status(200).json({ task });
 });
